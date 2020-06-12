58a81eb901de974f4a56c520b5348aed297f6ef3
jopetty <jacksonopetty@gmail.com>
2020-06-12 14:24:37 -0600

    Add command-line arguments to specify grammar and output files

--- generate_forms.py
+++ generate_forms.py
@@ -1,8 +1,7 @@
 # generate_forms.py
 #
-# This is the main file for the Logos project, a model which aims to translate
-# input sequences of natural language into logical forms, either ouput as 
-# sequences of some formal logic or as trees.
+# Generates a TSV file containing sentences and their logical forms generated
+# from the provided featural context-free grammar.
 
 import nltk
 from nltk.parse.generate import generate
@@ -18,20 +17,12 @@ def generate_f(grammar_file, out_file):
 		grammar = FeatureGrammar.fromstring(contents)
 
 		for sentence in generate(grammar):
-			sentences.append(' '.join(sentence))
+			sentences.append(' '.join(sentence).strip())
 
 	with open(out_file, 'w') as o:
 		for i, result in enumerate(nltk.interpret_sents(sentences, grammar_file)):
-			# print(result)
-			# type(type(result))
 			for (synrep, semrep) in result:
-				# s = semrep.uncurry()
-				# print(s)
-				# raise(SystemError)
-
-				# print(type(semrep))
-				# print('{0}'.format(semrep))
-				string = sentences[i].strip() + '\t' + '{0}'.format(semrep) + '\n'
+				string = sentences[i] + '\t' + '{0}'.format(semrep) + '\n'
 				o.write(string)
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-
+import argparse
 import generate_forms
 
+def getArguments():
+	"""
+	Instantiates an arugment parser, parses the command line arguments,
+	and returns them.
+
+	@return: args (argparse.Namespace) - command line arguments
+	"""
+	parser = argparse.ArgumentParser(description = 'Translate sentences into logical forms.')
+	parser.add_argument(
+		'-g', '--grammar',
+		type = str,
+		help = 'file containing grammar (FCFG) to generate logical forms',
+		default = 'grammar.fcfg'
+	)
+	parser.add_argument(
+		'-o', '--output',
+		type = str,
+		help = 'output file',
+		default = 'forms.txt'
+	)
+
+	args = parser.parse_args()
+	return args
+
 def main():
-	generate_forms.generate_f('grammar.fcfg', 'forms.txt')
+
+	args = getArguments()
+	generate_forms.generate_f(args.grammar, args.output)
 
 if __name__ == '__main__':
 	main()
