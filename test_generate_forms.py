ba6c60040dc6cb558f73b895ecda5a3f54d2bc58
jopetty <jacksonopetty@gmail.com>
2020-06-12 22:41:03 -0600

    Add whitespace tests for output file

--- test_generate_forms.py
+++ test_generate_forms.py
@@ -1,11 +1,16 @@
 import unittest
 import generate_forms
+import os
 
 class TestParsing(unittest.TestCase):
 
 	grammar_file = 'grammar.fcfg'
+	out_file = 'testing.txt'
 
 	def test_intransitive(self):
+		"""
+		Checks intransitive sentences.
+		"""
 		rubric = [
 			("Alice walks", "walk ( alice )"),
 			("Everyone walks", "all x . ( person ( x ) -> walk ( x ) )"),
@@ -17,6 +22,9 @@ class TestParsing(unittest.TestCase):
 		)
 
 	def test_transitive(self):
+		"""
+		Checks transitive sentences.
+		"""
 		rubric = [
 			("Claire sees Alice", "see ( claire , alice )"),
 			("Claire sees Everyone", "all x . ( person ( x ) -> see ( claire , x ) )"),
@@ -31,6 +39,16 @@ class TestParsing(unittest.TestCase):
 			[r[1] for r in rubric], 
 			generate_forms._generate_forms([r[0] for r in rubric], self.grammar_file)
 		)
+
+	def test_whitespace(self):
+		"""
+		Checks for extranous whitespace or formatting in the generate output file.
+		"""
+		generate_forms.generate_f(self.grammar_file, self.out_file)
+		with open(self.out_file, 'r') as f:
+			contents = f.readlines()
+			self.assertNotIn('  ', contents)
+		os.remove(self.out_file)
 
 if __name__ == '__main__':
 	unittest.main()
